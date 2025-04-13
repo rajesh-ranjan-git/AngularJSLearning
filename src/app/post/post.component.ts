9bbeaa260b630947288ad93ae3bba222d6d3792c
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2025-04-13 10:19:56 +0530

    lifecycle hooks

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -9,4 +9,36 @@ import { Component, input, output } from '@angular/core';
 export class PostComponent {
   postImage = input.required<string>();
   imageSelected = output<string>();
+
+  constructor() {
+    console.log('Constructor called');
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit called');
+  }
+
+  ngOnChanges() {
+    console.log('ngOnChanges called');
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck called');
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit called');
+  }
+
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked called');
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit called');
+  }
+
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked called');
+  }
 }
